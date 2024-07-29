e2d806afef591e7b0abb56c3516c00eafd3debad
ddavi01 <ddavi01@students.cs.ubc.ca>
2024-07-28 20:38:46 -0700

    created JFrame

---
+++ src/main/ui/Graphics.java
@@ -0,0 +1,23 @@
+package ui;
+
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+
+public class Graphics extends JFrame {
+
+    // public void myFrame() {
+    //     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //     this.setSize(500, 500);
+    //     this.setLayout(new FlowLayout());
+
+    //     JMenuBar menuBar = new JMenuBar();
+
+    //     JMenu fileMenu = new JMenu("File");
+
+    //     menuBar.add(fileMenu);
+    //     this.setMenuBar(menuBar);
+    //     this.setVisible(true);
+    // }
+
+}
--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,9 +1,25 @@
 package ui;
 
+import java.awt.Color;
 import java.io.FileNotFoundException;
 
-public class Main {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Main extends JFrame {
     public static void main(String[] args) {
+        JFrame frame = new JFrame();
+        frame.setTitle("Basil: The Recipe App");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.setResizable(false);
+        frame.setSize(1000,500);
+        frame.setVisible(true);
+
+        frame.getContentPane().setBackground(new Color(255,255,255));
+        ImageIcon image = new ImageIcon("icon.png");
+        frame.setIconImage(image.getImage());
+
         try {
             new RecipeApp();
         } catch (FileNotFoundException e) {
